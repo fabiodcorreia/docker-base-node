cdc37f57740e74f8cfbaaae24777f3630674e081
Fábio Correia <fabiodcorreia@gmail.com>
2020-07-12 19:47:19 +0100

    remove test for yarn

--- test/start.sh
+++ test/start.sh
@@ -2,7 +2,6 @@
 
 NODE=`node --version`
 NPM=`npm --version`
-YARN=`yarn --version`
 
 if [ "$NODE" != "v12.17.0" ]; then
   echo "**** Node not found - $NODE ****"
@@ -11,10 +10,5 @@ fi
 
 if [ "$NPM" != "6.14.4" ]; then
   echo "**** NPM not found - $NPM ****"
-  exit 1
-fi
-
-if [ "$YARN" != "1.22.4" ]; then
-  echo "**** Yarn not found - $NPM ****"
   exit 1
 fi
